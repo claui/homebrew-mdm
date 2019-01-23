b38a1f9025e6ee96640390312a2c8e57127ad370
Claudia <claui@users.noreply.github.com>
2019-01-23 15:18:52 +0100

    Update wlan-extern to 1.0.1

--- Casks/wlan-extern.rb
+++ Casks/wlan-extern.rb
@@ -1,10 +1,10 @@
 cask 'wlan-extern' do
-  version '1.0'
-  sha256 '2650d872691e6ef7a8d951a40154fb289aa1776cd6a75302ba9ad228078a78ff'
+  version '1.0.1'
+  sha256 'ad17c3df488872919b62756fdb24a7a16a0b8c0109ad7f9e99278e8808c78d09'
 
   # 802.1x-config.org was verified as official when first introduced to the cask
   url 'https://802.1x-config.org/user/API.php?action=downloadInstaller&lang=en&id=apple_macos&profile=226'
-  name 'Enterprise Network Configuration Proventa-WLAN für Externe'
+  name 'Proventa AG, Enterprise Network Configuration "externe MitarbeiterInnen"'
   homepage 'https://pm.proventa.io/projects/it-dokuwiki/wiki/Proventa_WLAN_macOS'
 
   conflicts_with cask: 'wlan-intern'
@@ -12,7 +12,7 @@ cask 'wlan-extern' do
   stage_only true
 
   preflight do
-    name = 'Enterprise_Network-macOS-Proventa_AG-proventa_ext_Wlan.mobileconfig'
+    name = 'Enterprise_Network-macOS-Proventa_AG-externe_MitarbeiterInnen.mobileconfig'
     FileUtils.mv(staged_path.children.first, staged_path / name)
     system_command '/usr/bin/profiles',
                    args: ['install', '-forced', '-path', staged_path / name],
@@ -25,7 +25,7 @@ cask 'wlan-extern' do
                                     'remove',
                                     '-forced',
                                     '-identifier',
-                                    'org.1x-config.enterprise_network.default.proventa_ag.proventa_ext_wlan.en_GB',
+                                    'org.1x-config.enterprise_network.default.proventa_ag.externe_mitarbeiterinnen.en_GB',
                                   ],
                       sudo:       true,
                     }
